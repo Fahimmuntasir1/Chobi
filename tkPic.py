7380be1048bb6a775eb706026a5446c6c9343720
Fahimmuntasir1 <fahimmuntasir043@gamil.com>
2024-05-11 14:06:41 +0600

    problem in opencv

--- tkPic.py
+++ tkPic.py
@@ -1,6 +1,11 @@
 # TKPiC - Image processing application.
 import cv2
 
+# read image from file
 img = cv2.imread("img/nature.jpg")
+
+imGray = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2GRAY)
+# to show image
 cv2.imshow("output", img)
+
 cv2.waitKey(0)
